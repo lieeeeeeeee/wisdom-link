bfae3b815768525115c6e7f1fc2897d387ed24df
lieeeeeeeee <qaz2wsx4edc6@icloud.com>
2025-05-15 11:51:53 +0900

    style(AudioPlayerControls): 再生ボタンのSVGに位置調整を追加

--- src/components/audio/AudioPlayerControls.tsx
+++ src/components/audio/AudioPlayerControls.tsx
@@ -37,7 +37,7 @@ const AudioPlayerControls = ({
               <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7-1a.75.75 0 00-.75.75v13.5a.75.75 0 00.75.75H16.5a.75.75 0 00.75-.75V5.25a.75.75 0 00-.75-.75H13.75z" clipRule="evenodd" />
             </svg>
           ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="currentColor" style={{ transform: 'translateX(1px)' }}>
               <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
             </svg>
           )}
